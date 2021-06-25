4b30372872fa69d46bcac1d57e748d3f2a46d173
Karan Parashar <whyredfire@gmail.com>
2021-06-25 11:14:45 +0530

    ginkgo: Welcome NezukoOS!
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2020 ArrowOS
+# Copyright (C) 2021 NezukoOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/nezuko_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    nezuko_ginkgo-user \
+    nezuko_ginkgo-userdebug \
+    nezuko_ginkgo-eng
